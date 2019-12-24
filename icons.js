2220c960d547b3003bf869a4508b0feb3a455bf6
Rael Garcia <rael@redhat.com>
2019-12-24 16:41:04 +0100

    Remove broken colorEmojiConfig dependency

--- icons.js
+++ icons.js
@@ -1,9 +1,7 @@
-const {readFileSync, writeFileSync} = require('fs');
-const {sync: glob} = require('glob');
-const {Logger, LogLevel} = require('plop-logger');
-const {colorEmojiConfig} = require('plop-logger/lib/extra/colorEmojiConfig');
+const { readFileSync, writeFileSync } = require('fs');
+const { sync: glob } = require('glob');
+const { Logger, LogLevel } = require('plop-logger');
 
-Logger.config = colorEmojiConfig;
 const logger = Logger.getLogger('icons');
 logger.level = LogLevel.All;
 
@@ -12,13 +10,13 @@ logger.info('SVG aggregation of', svgFiles);
 
 const data = glob(svgFiles)
   .map(file => {
-      const parts = file.split('/');
-      const id = parts[parts.length - 1].split('.')[0];
-      logger.debug('handle', id);
-      return readFileSync(file, 'utf8')
-        .replace(`<svg xmlns="http://www.w3.org/2000/svg"`, `\t<symbol id="${id}"`)
-        .replace(`</svg>`, `\t</symbol>`);
-    }
+    const parts = file.split('/');
+    const id = parts[parts.length - 1].split('.')[0];
+    logger.debug('handle', id);
+    return readFileSync(file, 'utf8')
+      .replace(`<svg xmlns="http://www.w3.org/2000/svg"`, `\t<symbol id="${id}"`)
+      .replace(`</svg>`, `\t</symbol>`);
+  }
   );
 
 const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" class="visually-hidden">
@@ -27,4 +25,4 @@ const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" class=
 
 const file = 'static/icons.svg';
 logger.info('Generate', file);
-writeFileSync(file, svg, {flag: 'w'});
+writeFileSync(file, svg, { flag: 'w' });
